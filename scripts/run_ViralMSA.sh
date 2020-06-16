3a0a805b59a0410dc31e07435b58b8b86edcfae5
Niema Moshiri <niemamoshiri@gmail.com>
2020-06-16 10:53:43 -0700

    Update run_ViralMSA.sh

--- scripts/run_ViralMSA.sh
+++ scripts/run_ViralMSA.sh
@@ -2,5 +2,4 @@
 if [ "$#" -ne 1 ] ; then
     echo "USAGE: $0 <fasta>" ; exit 1
 fi
-ViralMSA.py -t 8 -s $1 -r HIV1  > $1.mafft.aln 2> $1.mafft.log
 ViralMSA.py -s $1 -r HIV1 -e niemamoshiri@gmail.com -o $1.viralmsa > $1.viralmsa.log
